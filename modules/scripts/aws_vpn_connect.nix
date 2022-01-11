ea338022abc87428aef9ff35c6734de0ce9a5f6e
Abhishek Bansal <abhibansal530@gmail.com>
2022-01-11 11:55:06 +0530

    Escape $
    https://releases.nixos.org/nix-dev/2015-December/019018.html
    https://discourse.nixos.org/t/pkgs-writeshellscriptbin-error-syntax-error-unexpected/8405/2

--- modules/scripts/aws_vpn_connect.nix
+++ modules/scripts/aws_vpn_connect.nix
@@ -16,7 +16,7 @@ PROTO=udp
 
 wait_file() {
   local file="$1"; shift
-  local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
+  local wait_seconds="''${1:-10}"; shift # 10 seconds as default timeout
   until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; done
   ((++wait_seconds))
 }
@@ -25,7 +25,7 @@ wait_file() {
 RAND=$(openssl rand -hex 12)
 
 # resolv manually hostname to IP, as we have to keep persistent ip address
-SRV=$(dig a +short "${RAND}.${VPN_HOST}"|head -n1)
+SRV=$(dig a +short "''${RAND}.''${VPN_HOST}"|head -n1)
 
 # cleanup
 rm -f saml-response.txt
@@ -33,9 +33,9 @@ rm -f saml-response.txt
 # start the saml response server and background it
 go run server.go >> /tmp/aws-connect-saml-server.log 2>&1 &
 
-echo "Getting SAML redirect URL from the AUTH_FAILED response (host: ${SRV}:${PORT})"
-OVPN_OUT=$($OVPN_BIN --config "${OVPN_CONF}" --verb 3 \
-     --proto "$PROTO" --remote "${SRV}" "${PORT}" \
+echo "Getting SAML redirect URL from the AUTH_FAILED response (host: ''${SRV}:''${PORT})"
+OVPN_OUT=$($OVPN_BIN --config "''${OVPN_CONF}" --verb 3 \
+     --proto "$PROTO" --remote "''${SRV}" "''${PORT}" \
      --auth-user-pass <( printf "%s\n%s\n" "N/A" "ACS::35001" ) \
     2>&1 | grep AUTH_FAILED,CRV1)
 
@@ -43,7 +43,7 @@ echo "Opening browser and wait for the response file..."
 URL=$(echo "$OVPN_OUT" | grep -Eo 'https://.+')
 
 unameOut="$(uname -s)"
-case "${unameOut}" in
+case "''${unameOut}" in
     Linux*)     xdg-open "$URL";;
     Darwin*)    open "$URL";;
     *)          echo "Could not determine 'open' command for this OS"; exit 1;;
@@ -61,10 +61,10 @@ echo "Running OpenVPN with sudo. Enter password if requested"
 
 # Finally OpenVPN with a SAML response we got
 # Delete saml-response.txt after connect
-sudo bash -c "$OVPN_BIN --config "${OVPN_CONF}" \
+sudo bash -c "$OVPN_BIN --config "''${OVPN_CONF}" \
     --verb 3 --auth-nocache --inactive 3600 \
     --proto "$PROTO" --remote $SRV $PORT \
     --script-security 2 \
     --route-up '/usr/bin/env rm saml-response.txt' \
-    --auth-user-pass <( printf \"%s\n%s\n\" \"N/A\" \"CRV1::${VPN_SID}::$(cat saml-response.txt)\" )"
+    --auth-user-pass <( printf \"%s\n%s\n\" \"N/A\" \"CRV1::''${VPN_SID}::$(cat saml-response.txt)\" )"
 ''
