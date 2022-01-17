bee2052f75c7d1bcc9550a48bcbd89c24b7d8d65
Abhishek Bansal <abhibansal530@gmail.com>
2022-01-17 10:56:36 +0530

    Pulseaudio -> Pipewire

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -53,9 +53,8 @@ in
   };
 
   # Enable sound.
-  sound.enable = true;
   hardware.bluetooth.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -185,12 +184,43 @@ in
     };
   };
 
+  security.rtkit.enable = true;
   services = {
     blueman = {
       enable = true;
     };
     pipewire = {
       enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      media-session.config.bluez-monitor.rules = [
+        {
+          # Matches all cards
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+          actions = {
+            "update-props" = {
+              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+              # mSBC is not expected to work on all headset + adapter combinations.
+              "bluez5.msbc-support" = true;
+              # SBC-XQ is not expected to work on all headset + adapter combinations.
+              "bluez5.sbc-xq-support" = true;
+            };
+          };
+        }
+        {
+          matches = [
+            # Matches all sources
+            { "node.name" = "~bluez_input.*"; }
+            # Matches all outputs
+            { "node.name" = "~bluez_output.*"; }
+          ];
+          actions = {
+            "node.pause-on-idle" = false;
+          };
+        }
+      ];
     };
     resolved.enable = true;
   };
